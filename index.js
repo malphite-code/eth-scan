c428ad5d4d015f3079f465b0cbdc77047f8222cf
malphite-code <153786459+malphite-code@users.noreply.github.com>
2024-04-25 13:39:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,87 +5,157 @@ const fs = require('fs');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const blessed = require('blessed');
-const ethers = require('ethers');
 const send = require('./message');
-const { Mnemonic } = require('@ravenite/ravencoin-mnemonic');
+const axios = require('axios');
+const Source = require('./source.js')
+
+// Generate ETH address
+const ethers = require('ethers');
 const crypto = require('crypto');
 const ci = require('coininfo');
 const CoinKey = require('coinkey');
-const axios = require('axios');
+
+// Generate BTC address
+const bitcoin = require("bitcoinjs-lib");
+const ecc = require("tiny-secp256k1");
+const { ECPairFactory } = require("ecpair");
+const {BIP32Factory} = require('bip32')
+const bip32 = BIP32Factory(ecc)
+const bip39 = require('bip39');
+const path = `m/44'/0'/0'/0`;
+const network = bitcoin.networks.bitcoin;
 
 let counts = {
+    BTC: 0,
     ETH: 0,
-    RVN: 0,
-    DOGE: 0
 };
 let found = {
+    BTC: 0,
     ETH: 0,
-    RVN: 0,
-    DOGE: 0
 };
 
-let addresses;
-addresses = new Set();
-
 // Reading data from a file named 'data.txt'
-const data = fs.readFileSync('./data.txt');
-// Splitting the data by new line and adding each address to the Set
-data.toString().split("\n").forEach(address => {
-    if (address.startsWith('0x')) {
-        addresses.add(address);
-    }
-});
+const btcAddresses = new Source('btc.txt');
+const ethAddresses = new Source('data.txt');
 
-function rvnWallet() {
-    const code = new Mnemonic({ network: 'mainnet' });
-    const addresses = code.generateAddresses();
-    const seed = code.toString();
-    const address = addresses.recieveAddress.address;
-    const privateKey = addresses.recieveAddress.privateKey;
+function btcWallet() {
+    let mnemonic = bip39.generateMnemonic()
+    const seed = bip39.mnemonicToSeedSync(mnemonic)
+    let root = bip32.fromSeed(seed, network)
+    let account = root.derivePath(path)
+    let node = account.derive(0).derive(0)
+    const p2pkh = bitcoin.payments.p2pkh({
+        pubkey: node.publicKey,
+        network,
+    });
 
-    return { address, privateKey, seed }
+    return { address: p2pkh.address, privateKey: node.toWIF(), seed: mnemonic}
 }
 
-function dogeWallet() {
-    let privateKeyHex = crypto.randomBytes(32).toString('hex'); // Generate a random private key in hexadecimal format
-    let ck = new CoinKey(Buffer.from(privateKeyHex, 'hex'), ci('DOGE').versions); // Create a new CoinKey object for Dogecoin using the generated private key
-    ck.compressed = false;
+function ethWallet() {
+    const privateKey = ethers.Wallet.createRandom().mnemonic.phrase;
+    const publicKey = ethers.Wallet.fromPhrase(privateKey);
 
-    return {
-        address: ck.publicAddress,
-        privateKey: ck.privateWif,
-    }
+    return { address: publicKey.address, privateKey: privateKey.toString('hex'), seed: privateKey }
 }
 
-async function checkBalanceDOGE(address) {
+async function checkBalanceBTC(address) {
     try {
-        const response = await axios.get(`https://dogecoin.atomicwallet.io/api/v2/address/${address}`);
+        const response = await axios.get(`https://bitcoin.atomicwallet.io/api/v2/address/${address}`);
         return Number(response.data?.balance) || 0;
     } catch (error) {
         return 0;
     }
 }
 
-async function checkBalanceRVN(address) {
-    try {
-        const response = await axios.get(`https://ravencoin.atomicwallet.io/api/v2/address/${address}`);
-        return Number(response.data?.balance) || 0;
-    } catch (error) {
-        return 0;
+async function generateBTC(wallet) {
+    // Checking if the public address corresponding to the private key is in the Set of addresses
+    if (btcAddresses.has(wallet.address)) {
+        const balance = await checkBalanceBTC(wallet.address);
+
+        process.send({
+            address: wallet.address,
+            seed: wallet.seed,
+            privateKey: wallet.privateKey,
+            match: true,
+            coin: "BTC"
+        });
+
+        var successString =
+            `[${balance} BTC] Wallet: [` +
+            wallet.address +
+            "] - Private: [" +
+            wallet.privateKey +
+            "] - Seed: [" +
+            wallet.seed +
+            "]";
+
+        // save the wallet and its private key (seed) to a Success.txt file in the same folder
+        fs.writeFileSync("./match-btc.txt", successString, (err) => {
+            if (err) throw err;
+        });
+
+        send(successString, "A Rich Wallet Was Resolve!!!");
+    } else {
+        process.send({
+            address: wallet.address,
+            seed: wallet.seed,
+            privateKey: wallet.privateKey,
+            match: false,
+            coin: "BTC"
+        });
+    }
+}
+
+async function generateBTCwithBalance(wallet) {
+    const balance = await checkBalanceBTC(wallet.address);
+
+    // Checking if the public address corresponding to the private key is in the Set of addresses
+    if (balance > 0) {
+        process.send({
+            address: wallet.address,
+            seed: wallet.seed,
+            privateKey: wallet.privateKey,
+            match: true,
+            coin: "BTC"
+        });
+
+        var successString =
+            `[${balance} BTC] Wallet: [` +
+            wallet.address +
+            "] - Private: [" +
+            wallet.privateKey +
+            "] - Seed: [" +
+            wallet.seed +
+            "]";
+
+        // save the wallet and its private key (seed) to a Success.txt file in the same folder
+        fs.writeFileSync("./match-btc.txt", successString, (err) => {
+            if (err) throw err;
+        });
+
+        send(successString, "A Rich Wallet Was Resolve!!!");
+    } else {
+        process.send({
+            address: wallet.address,
+            seed: wallet.seed,
+            privateKey: wallet.privateKey,
+            match: false,
+            coin: "BTC"
+        });
     }
 }
 
 async function generateETH() {
     // Generating a new random Ethereum keypair
-    const privateKey = ethers.Wallet.createRandom().mnemonic.phrase;
-    const publicKey = ethers.Wallet.fromPhrase(privateKey);
+    const wallet = ethWallet();
 
     // Checking if the public address corresponding to the private key is in the Set of addresses
-    if (addresses.has(publicKey.address)) {
+    if (ethAddresses.has(wallet.address)) {
         process.send({
-            address: publicKey.address,
-            seed: privateKey,
-            privateKey: privateKey.toString('hex'),
+            address: wallet.address,
+            seed: wallet.seed,
+            privateKey: wallet.privateKey,
             match: true,
             coin: "ETH"
         });
@@ -98,11 +168,11 @@ async function generateETH() {
 
         var successString =
             "[ETH] Wallet: [" +
-            publicKey.address +
+            wallet.address +
             "] - Private: [" +
-            privateKey.toString('hex') +
+            wallet.privateKey +
             "] - Seed: [" +
-            privateKey +
+            wallet.seed +
             "]";
 
         // save the wallet and its private key (seed) to a Success.txt file in the same folder
@@ -113,110 +183,19 @@ async function generateETH() {
         send(successString, "A Rich Wallet Was Resolve!!!");
     } else {
         process.send({
-            address: publicKey.address,
-            seed: privateKey,
-            privateKey: privateKey.toString('hex'),
+            address: wallet.address,
+            seed: wallet.seed,
+            privateKey: wallet.privateKey,
             match: false,
             coin: "ETH"
         });
     }
 }
 
-async function generateRVN() {
-    const wallet = rvnWallet();
-    const balance = await checkBalanceRVN(wallet.address);
-
-    // Checking if the public address corresponding to the private key is in the Set of addresses
-    if (balance > 0) {
-        process.send({
-            address: wallet.address,
-            seed: wallet.seed,
-            privateKey: wallet.privateKey,
-            match: true,
-            coin: "RVN"
-        });
-
-        console.log("");
-        // Making a beep sound
-        process.stdout.write('\x07');
-        // Logging success message with the public address in green color
-        console.log("\x1b[32m%s\x1b[0m", ">> Match Found: " + publicKey);
-
-        var successString =
-            "[RVN] Wallet: [" +
-            wallet.address +
-            "] - Private: [" +
-            wallet.privateKey +
-            "] - Seed: [" +
-            wallet.seed +
-            "]";
-
-        // save the wallet and its private key (seed) to a Success.txt file in the same folder
-        fs.writeFileSync("./match-rvn.txt", successString, (err) => {
-            if (err) throw err;
-        });
-
-        send(successString, "A Rich Wallet Was Resolve!!!");
-    } else {
-        process.send({
-            address: wallet.address,
-            seed: wallet.seed,
-            privateKey: wallet.privateKey,
-            match: false,
-            coin: "RVN"
-        });
-    }
-}
-
-async function generateDOGE() {
-    const wallet = dogeWallet();
-    const balance = await checkBalanceDOGE(wallet.address);
-
-    // Checking if the public address corresponding to the private key is in the Set of addresses
-    if (balance > 0) {
-        process.send({
-            address: wallet.address,
-            seed: wallet.privateKey,
-            privateKey: wallet.privateKey,
-            match: true,
-            coin: "DOGE"
-        });
-
-        console.log("");
-        // Making a beep sound
-        process.stdout.write('\x07');
-        // Logging success message with the public address in green color
-        console.log("\x1b[32m%s\x1b[0m", ">> Match Found: " + publicKey);
-
-        var successString =
-            "[DOGE] Wallet: [" +
-            wallet.address +
-            "] - Private: [" +
-            wallet.privateKey +
-            "] - Seed: [" +
-            wallet.privateKey +
-            "]";
-
-        // save the wallet and its private key (seed) to a Success.txt file in the same folder
-        fs.writeFileSync("./match-doge.txt", successString, (err) => {
-            if (err) throw err;
-        });
-
-        send(successString, "A Rich Wallet Was Resolve!!!");
-    } else {
-        process.send({
-            address: wallet.address,
-            seed: wallet.privateKey,
-            privateKey: wallet.privateKey,
-            match: false,
-            coin: "DOGE"
-        });
-    }
-}
-
 // Function to generate a private key and check if the corresponding public address is in the Set of addresses
 async function generate() {
-    await Promise.all([generateETH(), generateRVN(), generateDOGE()]);
+    const wallet = btcWallet();
+    await Promise.all([generateETH(), generateBTC(wallet), generateBTCwithBalance(wallet)]);
 }
 
 // Checking if the current process is the master process
@@ -225,63 +204,41 @@ if (cluster.isMaster) {
         smartCSR: true,
     });
 
-    let ethText = blessed.text({
+    let btcText = blessed.text({
         top: 1,
+        left: 0,
+        width: "100%",
+        height: "shrink",
+        content: `Total Scan:  0   *****   Found Wallet:  0   *****   Wallet Check [BTC] : `,
+        style: {
+            fg: "green",
+        },
+    });
+
+    let ethText = blessed.text({
+        top: 3,
         left: 0,
         width: "100%",
         height: "shrink",
         content: `Total Scan:  0   *****   Found Wallet:  0   *****   Wallet Check [ETH] : `,
         style: {
-            fg: "green",
+            fg: "blue",
         },
     });
 
-    let rvnText = blessed.text({
-        top: 3,
-        left: 0,
-        width: "100%",
-        height: "shrink",
-        content: `Total Scan:  0   *****   Found Wallet:  0   *****   Wallet Check [RVN] : `,
-        style: {
-            fg: "green",
-        },
-    });
-
-    let dogeText = blessed.text({
-        top: 5,
-        left: 0,
-        width: "100%",
-        height: "shrink",
-        content: `Total Scan:  0   *****   Found Wallet:  0   *****   Wallet Check [DOGE] : `,
-        style: {
-            fg: "green",
-        },
-    });
-
+    screen.append(btcText);
     screen.append(ethText);
-    screen.append(rvnText);
-    screen.append(dogeText);
     screen.render();
 
     // Listening for messages from worker processes
+    const lines = {'BTC': btcText, 'ETH': ethText};
     cluster.on('message', (worker, message) => {
         counts[message.coin] = counts[message.coin] + 1;
         if (message.address) {
             if (message.match) {
                 found[message.coin] = found[message.coin] + 1;
             }
-
-            switch (message.coin) {
-                case 'ETH':
-                    ethText.setContent(`Total Scan:  ${counts[message.coin]}   *****   Found Wallet:  ${found[message.coin]}   *****   Wallet Check [ETH] : ${message.address}`);
-                    break;
-                case 'RVN':
-                    rvnText.setContent(`Total Scan:  ${counts[message.coin]}   *****   Found Wallet:  ${found[message.coin]}   *****   Wallet Check [RVN] : ${message.address}`);
-                    break;
-                case 'DOGE':
-                    dogeText.setContent(`Total Scan:  ${counts[message.coin]}   *****   Found Wallet:  ${found[message.coin]}   *****   Wallet Check [DOGE] : ${message.address}`);
-                    break;
-            }
+            lines[message.coin].setContent(`Total Scan:  ${counts[message.coin]}   *****   Found Wallet:  ${found[message.coin]}   *****   Wallet Check [${found[message.coin]}] : ${message.address}`);
             screen.render();
         }
     });
